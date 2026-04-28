fd6a41691d5966ae510907d0556d38ab36dd6e41
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-04-28 19:21:57 +0300

    feat: add output variables

--- src/app/components/step7-list-group-menu/step7-list-group-menu.ts
+++ src/app/components/step7-list-group-menu/step7-list-group-menu.ts
@@ -13,6 +13,7 @@ export class Step7ListGroupMenu {
     {text: "Step3 Component Input", appLink: 'component-input-example'},
     {text: "Step 4 @for Directive", appLink: 'for-directive-example'},
     {text: "Step5 Event Bind", appLink: 'event-bind-example'},
-    {text: "Step 8 Simple Data Table", appLink: 'simple-datatable-example'}   
+    {text: "Step 8 Simple Data Table", appLink: 'simple-datatable-example'},
+    {text: "Step 9 Output Example", appLink: 'component-output-example'}   
   ]
 }
