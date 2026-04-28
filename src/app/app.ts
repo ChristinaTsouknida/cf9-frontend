1e865f15b2716580107583f2e3d59385af245712
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-04-28 18:04:12 +0300

    feat: main

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Step7ListGroupMenu } from './components/step7-list-group-menu/step7-list-group-menu';
 import { Step2PersonTable } from './components/step2-person-table/step2-person-table';
 import { Step3PersonTableInput } from './components/step3-person-table-input/step3-person-table-input';
+import { Step4ForDirective } from './components/step4-for-directive/step4-for-directive';
+import { Step5EventBind } from './components/step5-event-bind/step5-event-bind';
 import { IPerson } from './shared/interfaces/person';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Step2PersonTable, Step3PersonTableInput],
+  imports: [
+      RouterOutlet,
+      Navbar,
+      Step7ListGroupMenu,
+      Step2PersonTable,
+      Step3PersonTableInput,
+      Step4ForDirective,
+      Step5EventBind],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -30,4 +42,6 @@ export class App {
     lastname: 'lastname2',
     email: 'lastname2@aueb.gr'
   }
+
+  doNotShowComponents:boolean = true;
 }
